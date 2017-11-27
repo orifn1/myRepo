1d37278a00fc008c7c7bd44a6f08819619114973
yaksyutin <aksyutin_yurij@outlook.com>
2017-11-27 21:48:10 +0200

    project

---
+++ Experiment/Experiment/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Experiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.Read();
+        }
+    }
+}
